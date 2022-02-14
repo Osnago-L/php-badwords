7a38e099956c15027b8594f5e353d2bcb5a02b82
Osnago-L <osna.brera@gmail.com>
2022-02-14 16:05:21 +0100

    end repo

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
-    $string = "Ciao, sono Lorenzo"
-
+    $string = "Ci sarà un giorno in cui il coraggio degli uomini cederà, in cui abbandoneremo gli amici e spezzeremo ogni legame di fratellanza, ma non è questo il giorno! Ci sarà l’ora dei lupi e degli scudi frantumati quando l’era degli uomini arriverà al crollo, ma non è questo il giorno! Quest’oggi combattiamo. Per tutto ciò che ritenete caro su questa bella Terra, vi invito a resistere! Uomini dell’ovest!";
+    $len = strlen ($string);
+    $userString = $_GET["censor"];
+    $tocensor = "***";
+    
+    $string = str_replace($userString,$tocensor,$string);
 ?>
 
-<h1></h1>
+<p>(<?php echo $len?>)</p>
+<p><?php echo $string?></p>
